dcb65006738949b2b2ea7f0b1e1763cc59bd44c9
coder-xsj <fibonacci701408733@163.com>
2021-12-19 14:41:36 +0800

    topicList component

--- src/api/topic.js
+++ src/api/topic.js
@@ -9,3 +9,15 @@ export function getTopics(data) {
 export function getCategories(data) {
   return request('categories')
 }
+
+export function getTopic(id, data) {
+  return request('topics/' + id, {
+    data: data
+  })
+}
+
+export function getUserTopics(userId, data) {
+  return request('users/'+userId+'/topics', {
+    data: data
+  })
+}
