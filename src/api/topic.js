86a095ce081e2f2cad2a90db36f65037381d2ebe
coder-xsj <fibonacci701408733@163.com>
2022-05-17 21:01:37 +0800

    删除帖子

--- src/api/topic.js
+++ src/api/topic.js
@@ -1,4 +1,4 @@
-import { request } from '@/utils/request'
+import { request, authRequest } from '@/utils/request'
 
 export function getTopics(data) {
   return request('topics', {
@@ -18,6 +18,14 @@ export function getTopic(id, data) {
 
 export function getUserTopics(userId, data) {
   return request('users/'+userId+'/topics', {
+    data: data
+  })
+}
+
+// 删除帖子
+export function deleteTopic(id, data) {
+  return authRequest('topics/' + id, {
+    method: 'DELETE',
     data: data
   })
 }
